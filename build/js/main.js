ed22afe21e0e4b9a9a0e438d4cca8cbfdb24fc04
Sofia Puch <sophiepuch@gmail.com>
2016-06-07 11:00:04 +0100

    added spinner while loading pictures and changes in markup

--- build/js/main.js
+++ build/js/main.js
@@ -2,25 +2,39 @@ angular.module("runpathTest", [])
 
 	.controller("runpathTestCtrl", ['$scope', '$http', function ($scope, $http) {
 
+		$scope.loading = false;
 		$scope.galleryUrl = 'http://jsonplaceholder.typicode.com/photos';
 
+
+		//load the images for the API
 		$scope.loadImages = function () {
 
+			$scope.loading = true;
 
 			$http.get($scope.galleryUrl)
 				.then(function (response) {
-		        	console.log("data: ", response.data);
+
+					$scope.displayImages(response.data);
+					$scope.loading = false;
+
 		    	}, function () {
 		    		console.log("error loading the data");
+		    		$scope.loading = false;
 		    	}
 		    );
+		};
+
+
+		$scope.displayImages = function (images, page) {
+
+			$scope.images = images;
+			//pagination
+
+			console.log();
 
 
 
 		};
-
-
-
 
 
 
